3717909c6b87e2bc8c8d5573b86c80e8dc21c702
chetan <chetan.d49@gmail.com>
2023-01-15 19:24:19 +0530

    home page implimention

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componets/header/header.component';
+import { FooterComponent } from './Componets/footer/footer.component';
+import { HomeComponent } from './Componets/home/home.component';
+import { PageNotFoundComponent } from './Componets/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
