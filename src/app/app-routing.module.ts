9961d16cf19edf44fcbc0ab8728f28964dabc044
chetan <chetan.d49@gmail.com>
2023-01-19 20:48:00 +0530

    top deal added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Componets/home/home.component';
+import { MedicinesHomeComponent } from './Componets/medicines-home/medicines-home.component';
 import { PageNotFoundComponent } from './Componets/page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
   {path : "Home" , component:HomeComponent},
+  {path:"Medicine", component:MedicinesHomeComponent},
   {path:"", redirectTo:"/Home",pathMatch:'full'},
   {path: "**", component:PageNotFoundComponent}
 
